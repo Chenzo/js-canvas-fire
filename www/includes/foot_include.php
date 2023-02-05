1405a6f4615c8797ee1571d9d3de340302138616
Chenzorama <chenzorama@hotmail.com>
2023-02-05 13:49:37 -0500

    nextjsified

--- www/includes/foot_include.php
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="/js/StackBlur.js"></script>
-
-<script type="text/javascript" src="/js/firejs.js?v=<?=$cacheBusterNumber; ?>"></script>
-
-
-<script type="text/javascript">
-	//\
-</script>
